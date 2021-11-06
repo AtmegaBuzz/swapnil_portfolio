aa577eb642e47addba6f4ae937664011318763ee
AtmegaBuzz <creepydazze@gmail.com>
2021-11-06 09:56:50 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -20,13 +20,13 @@ export default function MyApp({Component,pageProps}){
             <meta name="twitter:description" content="swapnil shinde personal portfolio | cyberspoof"/>
             <meta name="url" content="https://cyberspoof.me"/>
             <meta name="twitter:url" content="https://cyberspoof.me"/>
-            <meta property="og:url" content="https://cyberspoof.me"/>
+            <meta property="og:url" content="https://instagram.fbom15-1.fna.fbcdn.net/v/t51.2885-19/s150x150/253629615_2151395878346309_1761419598058685721_n.jpg?_nc_ht=instagram.fbom15-1.fna.fbcdn.net&_nc_ohc=bf0Zjl-O_coAX-ux3Ad&edm=ALbqBD0BAAAA&ccb=7-4&oh=c28b71590ca579637540e6808c11b3bb&oe=618DA439&_nc_sid=9a90d6"/>
             <meta property="og:locale" content="en"/>
             <meta  name="language" content="en"/>
-            <meta itemProp="image" content="https://cyberspoof.me/_next/image?url=%2Flogo.png&w=96&q=75"/>
-            <meta property="og:image" content="https://cyberspoof.me/_next/image?url=%2Flogo.png&w=96&q=75"/>
-            <meta name="twitter:image" content="https://cyberspoof.me/_next/image?url=%2Flogo.png&w=96&q=75"/>
-            <meta name="twitter:image:src" content="https://cyberspoof.me/_next/image?url=%2Flogo.png&w=96&q=75"/>
+            <meta itemProp="image" content="https://instagram.fbom15-1.fna.fbcdn.net/v/t51.2885-19/s150x150/253629615_2151395878346309_1761419598058685721_n.jpg?_nc_ht=instagram.fbom15-1.fna.fbcdn.net&_nc_ohc=bf0Zjl-O_coAX-ux3Ad&edm=ALbqBD0BAAAA&ccb=7-4&oh=c28b71590ca579637540e6808c11b3bb&oe=618DA439&_nc_sid=9a90d6"/>
+            <meta property="og:image" content="https://instagram.fbom15-1.fna.fbcdn.net/v/t51.2885-19/s150x150/253629615_2151395878346309_1761419598058685721_n.jpg?_nc_ht=instagram.fbom15-1.fna.fbcdn.net&_nc_ohc=bf0Zjl-O_coAX-ux3Ad&edm=ALbqBD0BAAAA&ccb=7-4&oh=c28b71590ca579637540e6808c11b3bb&oe=618DA439&_nc_sid=9a90d6"/>
+            <meta name="twitter:image" content="https://instagram.fbom15-1.fna.fbcdn.net/v/t51.2885-19/s150x150/253629615_2151395878346309_1761419598058685721_n.jpg?_nc_ht=instagram.fbom15-1.fna.fbcdn.net&_nc_ohc=bf0Zjl-O_coAX-ux3Ad&edm=ALbqBD0BAAAA&ccb=7-4&oh=c28b71590ca579637540e6808c11b3bb&oe=618DA439&_nc_sid=9a90d6"/>
+            <meta name="twitter:image:src" content="https://instagram.fbom15-1.fna.fbcdn.net/v/t51.2885-19/s150x150/253629615_2151395878346309_1761419598058685721_n.jpg?_nc_ht=instagram.fbom15-1.fna.fbcdn.net&_nc_ohc=bf0Zjl-O_coAX-ux3Ad&edm=ALbqBD0BAAAA&ccb=7-4&oh=c28b71590ca579637540e6808c11b3bb&oe=618DA439&_nc_sid=9a90d6"/>
             <meta name="twitter:image:alt" content="Feature Image cyberspoof"/>
             <title itemprop="name">CyberSpoof | swapnil shinde personal portfolio</title> 
 
