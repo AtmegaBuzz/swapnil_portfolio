306ea203b9530929f2fc26e436e67e5a746f2c34
AtmegaBuzz <creepydazze@gmail.com>
2021-11-08 19:40:59 +0530

    Update about.jsx

--- components/about/about.jsx
+++ components/about/about.jsx
@@ -15,7 +15,7 @@ export default function About(){
                 <div className={styles.about_cont}>
                     <div className={styles.heading}>About Me</div>
                     <div className={styles.about_content}>
-                        <p className={styles.sub_about}>Hi,My name is Swapnil Shinde and Im a Fullstack Developer and a Cyber Security enthusiast from Maharashtra, India</p>
+                        <p className={styles.sub_about}>Hi,My name is Swapnil Shinde and Im a Fullstack Developer which you can see on my face and a Cyber Security enthusiast from Maharashtra, India</p>
                         <p className={styles.sub_sub_about}>Interest in Cyber Security and Understood OWASP top 10 which helped me in developing secure backends.</p>
                         <p className={styles.sub_sub_about}>I write about Cyber Security on my <a target="_blank" href="https://www.instagram.com/cyber.spoof/">instagram</a> blogpost to share my knowledge and to contribute in spreading awerness.</p>
                         <p className={styles.sub_sub_about}>I'm a fullstack developer specialised in frontend (<a target="_blank" href="https://reactjs.org/">react</a>) and backend (<a target="_blank" href="https://www.djangoproject.com/">django</a>).</p>
